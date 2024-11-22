35230c9f350c53d08c3dc7ae73461db86e6137ad
louxfaure <louxfaure@gmail.com>
2024-11-22 09:05:01 +0100

    modif logs

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import bib, autres_parametres
 
 SERVICE = "Alma_Suivi_Import_Notice_SUDOC"
 
-LOGS_LEVEL = 'DEBUG'
+LOGS_LEVEL = 'INFO'
 LOGS_DIR = os.getenv('LOGS_PATH')
 JOB_ID = 'S10555924020004671' #Identifiant du profil d'import
 API_KEY = os.getenv('PROD_NETWORK_BIB_API') 
